6c9fabf78f6c17a15de18d9476e231bd34f1adb3
sangzun-han <hsjun1996@naver.com>
2021-09-15 19:18:35 +0900

    ADD : folder structure

--- SocialMedia/socialmedia/README.md
+++ SocialMedia/socialmedia/README.md
@@ -2,3 +2,9 @@
 - Notification and newsfeed
 - Easy integration with other platforms
 - Posting, sharing and commenting
+
+```
+npm install date-fns
+npm install firebase
+npm install react-loading-skeleton
+```
--- SocialMedia/socialmedia/src/App.js
+++ SocialMedia/socialmedia/src/App.js
@@ -1,12 +1,5 @@
-import './App.css';
-
-import Header from './components/Header';
 function App() {
-  return (
-    <div className='App'>
-      <Header />
-    </div>
-  );
+  return <p className='id'>Hello</p>;
 }
 
 export default App;

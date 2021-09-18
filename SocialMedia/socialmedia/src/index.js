fb955436af57facbc22b38726c9f55951e4f14da
sangzun-han <hsjun1996@naver.com>
2021-09-18 10:57:51 +0900

    ADD : woring on Login Page

--- SocialMedia/socialmedia/src/index.js
+++ SocialMedia/socialmedia/src/index.js
@@ -3,26 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import FirebaseContext from './context/firebase';
 import { firebase, FieldValue } from './lib/firebase';
+import './styles/app.css';
+
 ReactDOM.render(
   <FirebaseContext.Provider value={{ firebase, FieldValue }}>
     <App />
   </FirebaseContext.Provider>,
   document.getElementById('root')
 );
-
-// client side renderd app: react (cra)
-// -> database which is Firebase
-// -> react-loading-skeleton
-// tailwind
-
-// folder architecture
-// src
-// -> components,
-// -> constants,
-// -> context,
-// -> helpers
-// -> hooks,
-// -> pages,
-// -> lib (firebase is going to live in here),
-// -> services(firebase functions in here)
-// -> styles (tailwind's folder (app/tailwind))
---
+++ SocialMedia/socialmedia/src/pages/login.js
@@ -0,0 +1,69 @@
+import { useContext, useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import FirebaseContext from '../context/firebase';
+
+export default function Login() {
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
+
+  const [emailAddress, setEmailAddress] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [error, setError] = useState('');
+  const isInvalid = password === '' || emailAddress === '';
+
+  const handleLogin = () => {};
+
+  useEffect(() => {
+    document.title = 'Login - Instagram';
+  }, []);
+  return (
+    <div className='container flex mx-auto max-w-screen-md items-center h-screen'>
+      <div className='flex w-3/5'>
+        <img
+          src='images/iphone-with-profile.jpeg'
+          alt='iPhone with Instagram app'
+        />
+      </div>
+      <div className='flex flex-col w2-/5'>
+        <h1 className='flex justify-center w-full'>
+          <img
+            src='/images/logo.png'
+            alt='Instagram'
+            className='mt-2 w-6/12 mb-4'
+          />
+        </h1>
+        {error && <p className='mb-4 text-xs text-red-primary'>{error}</p>}
+
+        <form onSubmit={handleLogin} method='POST'>
+          <input
+            area-label='Enter your email address'
+            type='text'
+            placeholder='이메일'
+            className='text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2'
+            onChange={({ target }) => setEmailAddress(target.value)}
+          />
+          <input
+            area-label='Enter your password'
+            type='password'
+            placeholder='비밀번호'
+            className='text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2'
+            onChange={({ target }) => setPassword(target.value)}
+          />
+          <button
+            disabled={isInvalid}
+            type='submit'
+            className={`bg-blue-medium text-white w-full rounded h-8 font-bold ${
+              isInvalid && 'opacity-50'
+            }`}
+          >
+            Log In
+          </button>
+        </form>
+      </div>
+      <div className='flex justify-center items-center flex-col w-full bg-white p-4 border border-gray-primary'>
+        <p className='text-sm'>Don't have an account?</p>
+      </div>
+    </div>
+  );
+}

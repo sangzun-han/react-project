28355f9fad3c886d245a8fa7d472c5ca22f263c5
sangzun-han <hsjun1996@naver.com>
2021-09-18 15:07:26 +0900

    Update : Login auth with firebase

--- SocialMedia/socialmedia/tailwind.config.js
+++ SocialMedia/socialmedia/tailwind.config.js
@@ -2,4 +2,28 @@ module.exports = {
   future: {
     removeDeprecatedGapUtilities: true,
   },
+
+  theme: {
+    fill: (theme) => ({
+      red: theme('colors.red.primary'),
+    }),
+    colors: {
+      white: '#ffffff',
+      blue: {
+        medium: '#005c98',
+      },
+      black: {
+        medium: '#005c98',
+        faded: '#00000059',
+      },
+      gray: {
+        base: '#616161',
+        background: '#fafafa',
+        primary: '#dbdbdb',
+      },
+      red: {
+        primary: '#ed4956',
+      },
+    },
+  },
 };

2274a7ea3b7d46ecf9bad3e1b44349574750f58f
sangzun-han <hsjun1996@naver.com>
2021-09-26 21:55:47 +0900

    ADD : remove followed user

--- SocialMedia/socialmedia/src/components/sidebar/suggestions.js
+++ SocialMedia/socialmedia/src/components/sidebar/suggestions.js
@@ -1,3 +1,55 @@
-export default function Suggestions() {
-  return <p>suggestions</p>;
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import Skeleton from 'react-loading-skeleton';
+import { getSuggestedProfiles } from '../../services/firebase';
+import SuggestedProfile from './suggestied-profile';
+
+export default function Suggestions({ userId, following }) {
+  const [profiles, setProfiles] = useState(null);
+
+  // go ahead and get the suggesteed profiles
+  useEffect(() => {
+    async function suggestedProfiles() {
+      const response = await getSuggestedProfiles(userId, following);
+      setProfiles(response);
+    }
+
+    if (userId) {
+      suggestedProfiles();
+    }
+
+    getSuggestedProfiles();
+  }, [userId]);
+
+  // use the firebase service
+  // getSuggestedProfiles
+  // call the async function
+  // store it in state
+  // go ahead and render
+
+  return !profiles ? (
+    <Skeleton count={1} height={150} className='mt-5' />
+  ) : profiles.length > 0 ? (
+    <div className='rounded flex flex-col'>
+      <div className='text-sm flex items-center align-items justify-between mb-2'>
+        <p className='font-bold text-gray-base'>Suggestions for you</p>
+      </div>
+      <div className='mt-4 gird gap-5'>
+        {profiles.map((profile) => (
+          <SuggestedProfile
+            key={profile.docId}
+            userDocId={profile.docId}
+            username={profile.username}
+            profileId={profile.userId}
+            userId={userId}
+          />
+        ))}
+      </div>
+    </div>
+  ) : null;
 }
+
+Suggestions.propTypes = {
+  userId: PropTypes.string,
+  following: PropTypes.array,
+};

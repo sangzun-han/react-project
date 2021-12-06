51f607a99d1387c24499ceb554ed956414e65935
sangzun-han <hsjun1996@naver.com>
2021-12-07 00:23:02 +0900

    Clean up Habits Component

--- habit-tracker/src/app.jsx
+++ habit-tracker/src/app.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./app.css";
 import Habits from "./components/habits";
+import Navbar from "./components/navbar";
 
 class App extends Component {
   state = {
@@ -10,27 +11,41 @@ class App extends Component {
       { id: 3, name: "Coding", count: 0 },
     ],
   };
+
+  handleIncrement = (habit) => {
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    habits[index].count++;
+    this.setState({ habits });
+  };
+
+  handleDecrement = (habit) => {
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    const count = habits[index].count - 1;
+    habits[index].count = count < 0 ? 0 : count;
+    this.setState({ habits });
+  };
+
+  handleDelete = (habit) => {
+    const habits = this.state.habits.filter((item) => item.id !== habit.id);
+    this.setState({ habits });
+  };
+
   render() {
-    handleIncrement = (habit) => {
-      const habits = [...this.state.habits];
-      const index = habits.indexOf(habit);
-      habits[index].count++;
-      this.setState({ habits });
-    };
-
-    handleDecrement = (habit) => {
-      const habits = [...this.state.habits];
-      const index = habits.indexOf(habit);
-      habits[index].count - 1;
-      habits[index].count = count < 0 ? 0 : count;
-      this.setState({ habits });
-    };
-
-    handleDelete = (habit) => {
-      const habits = this.state.habits.filter((item) => item.id);
-    };
-
-    return <Habits />;
+    return (
+      <>
+        <Navbar
+          totalCount={this.state.habits.filter((item) => item.count > 0).length}
+        />
+        <Habits
+          habits={this.state.habits}
+          onIncrement={this.handleIncrement}
+          onDecrement={this.handleDecrement}
+          onDelete={this.handleDelete}
+        />
+      </>
+    );
   }
 }
 
--- habit-tracker/src/components/habit.jsx
+++ habit-tracker/src/components/habit.jsx
@@ -2,24 +2,43 @@ import React, { Component } from "react";
 
 class Habit extends Component {
   handleIncrement = (habit) => {
-    this.props.onIncrement(habit);
+    this.props.onIncrement(this.props.habit);
   };
 
   handleDecrement = (habit) => {
-    this.props.onDecrement(habit);
+    this.props.onDecrement(this.props.habit);
   };
 
   handleDelete = (habit) => {
-    this.props.onDelete(habit);
+    this.props.onDelete(this.props.habit);
   };
 
   render() {
+    const { name, count } = this.props.habit;
     return (
-      <ul>
-        {this.props.habits.map((habit) => (
-          <Habit />
-        ))}
-      </ul>
+      <li className="habit">
+        <span className="habit-name">{name}</span>
+        <span className="habit-count">{count}</span>
+        <button
+          className="habit-button habit-increase"
+          onClick={this.handleIncrement}
+        >
+          <i className="fas fa-plus"></i>
+        </button>
+        <button
+          className="habit-button habit-decrease"
+          onClick={this.handleDecrement}
+        >
+          <i className="fas fa-minus"></i>
+        </button>
+
+        <button
+          className="habit-button habit-delete"
+          onClick={this.handleDelete}
+        >
+          <i className="fas fa-trash"></i>
+        </button>
+      </li>
     );
   }
 }

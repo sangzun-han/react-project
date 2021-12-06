b4870168a1a5d4744f92c8c36131fd25326ed8c3
sangzun-han <hsjun1996@naver.com>
2021-12-06 23:58:26 +0900

    Clean up Habits Component

--- habit-tracker/src/app.jsx
+++ habit-tracker/src/app.jsx
@@ -1,7 +1,37 @@
-import Habits from "./components/habits";
+import React, { Component } from "react";
 import "./app.css";
-function App() {
-  return <Habits />;
+import Habits from "./components/habits";
+
+class App extends Component {
+  state = {
+    habits: [
+      { id: 1, name: "Reading", count: 0 },
+      { id: 2, name: "Running", count: 0 },
+      { id: 3, name: "Coding", count: 0 },
+    ],
+  };
+  render() {
+    handleIncrement = (habit) => {
+      const habits = [...this.state.habits];
+      const index = habits.indexOf(habit);
+      habits[index].count++;
+      this.setState({ habits });
+    };
+
+    handleDecrement = (habit) => {
+      const habits = [...this.state.habits];
+      const index = habits.indexOf(habit);
+      habits[index].count - 1;
+      habits[index].count = count < 0 ? 0 : count;
+      this.setState({ habits });
+    };
+
+    handleDelete = (habit) => {
+      const habits = this.state.habits.filter((item) => item.id);
+    };
+
+    return <Habits />;
+  }
 }
 
 export default App;
--- habit-tracker/src/components/habit.jsx
+++ habit-tracker/src/components/habit.jsx
@@ -1,44 +1,25 @@
 import React, { Component } from "react";
 
 class Habit extends Component {
-  handleIncrement = () => {
-    this.props.onIncrement(this.props.habit);
+  handleIncrement = (habit) => {
+    this.props.onIncrement(habit);
   };
 
-  handleDecrement = () => {
-    this.props.onDecrement(this.props.habit);
+  handleDecrement = (habit) => {
+    this.props.onDecrement(habit);
   };
 
-  handleDelete = () => {
-    this.props.onDelete(this.props.habit);
+  handleDelete = (habit) => {
+    this.props.onDelete(habit);
   };
 
   render() {
-    const { name, count } = this.props.habit;
     return (
-      <li className="habit">
-        <span className="habit-name">{name}</span>
-        <span className="habit-count">{count}</span>
-        <button
-          className="habit-button habit-increase"
-          onClick={this.handleIncrement}
-        >
-          <i className="fas fa-plus"></i>
-        </button>
-        <button
-          className="habit-button habit-decrease"
-          onClick={this.handleDecrement}
-        >
-          <i className="fas fa-minus"></i>
-        </button>
-
-        <button
-          className="habit-button habit-delete"
-          onClick={this.handleDelete}
-        >
-          <i className="fas fa-trash"></i>
-        </button>
-      </li>
+      <ul>
+        {this.props.habits.map((habit) => (
+          <Habit />
+        ))}
+      </ul>
     );
   }
 }

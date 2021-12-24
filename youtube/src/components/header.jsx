9c10e3e4d1e8aa0384face656ab45dcffe7a2cc4
sangzun-han <hsjun1996@naver.com>
2021-12-24 16:50:56 +0900

    youtube header

--- youtube/src/components/header.jsx
+++ youtube/src/components/header.jsx
@@ -1,7 +1,27 @@
 import React from "react";
-
+import styles from "../styles/header.module.css";
+import { faBell, faUser } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className={styles.header}>
+      <ul className={styles.navbar}>
+        <li>
+          <img src="images/logo.png" alt="logo" />
+        </li>
+        <li className={styles.searchInput}>
+          <input type="text" placeholder="검색" className={styles.input} />
+          <button type="submit" className={styles.searchButton}>
+            <img src="images/search.png" alt="search" className={styles.logo} />
+          </button>
+        </li>
+        <li className={styles.icon}>
+          <FontAwesomeIcon icon={faBell} size="2x" />
+          <FontAwesomeIcon icon={faUser} size="2x" />
+        </li>
+      </ul>
+    </header>
+  );
 };
 
 export default Header;

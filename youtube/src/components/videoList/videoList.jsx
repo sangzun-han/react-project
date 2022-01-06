d8ea249f6c91d0de24effc300789375bc225773e
sangzun-han <hsjun1996@naver.com>
2022-01-06 14:31:05 +0900

    styling video

--- youtube/src/components/videoList/videoList.jsx
+++ youtube/src/components/videoList/videoList.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import VideoItem from "../videoItem/videoItem";
+import styles from "./videoList.module.css";
 
 const VideoList = (props) => {
   return (
-    <ul>
+    <ul className={styles.videos}>
       {props.videos.map((video) => (
         <VideoItem key={video.id} video={video} />
       ))}

93a0d4f8f74dc1c898e11529a422ee1cd5e42a62
sangzun-han <hsjun1996@naver.com>
2022-01-06 14:12:47 +0900

    show video list

--- youtube/src/app.jsx
+++ youtube/src/app.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import VideoList from "./components/videoList/videoList";
 
 function App() {
-  return <h1>Hello</h1>;
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyBWIoBHpp-7lbyWn3PihM59OVGIlGFShuY",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log("error", error));
+  }, []);
+  return <VideoList videos={videos} />;
 }
 
 export default App;

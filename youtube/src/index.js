47e4b34d5cca7e10f1b1889abebdbeb70acaa720
sangzun-han <hsjun1996@naver.com>
2022-01-08 10:21:54 +0900

    fetch -> axios

--- youtube/src/index.js
+++ youtube/src/index.js
@@ -2,9 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./app";
-import Youtube from "./utils/fetcher";
+import Youtube from "./utils/youtube";
+import axios from "axios";
 
-const youtube = new Youtube(process.env.REACT_APP_YOUTUBE_API_KEY);
+const httpClient = axios.create({
+  baseURL: "https://youtube.googleapis.com/youtube/v3",
+  params: { key: process.env.REACT_APP_YOUTUBE_API_KEY },
+});
+
+const youtube = new Youtube(httpClient);
 ReactDOM.render(
   <React.StrictMode>
     <App youtube={youtube} />

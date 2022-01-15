c988f3a24c798ee7ac2e717af6f00e51cf7dcfa0
sangzun-han <hsjun1996@naver.com>
2022-01-15 11:52:56 +0900

    add loading spinner

--- card-maker/src/components/imageFileInput/imageFileInput.jsx
+++ card-maker/src/components/imageFileInput/imageFileInput.jsx
@@ -1,7 +1,11 @@
+import { keyframes } from "@emotion/react";
 import styled from "@emotion/styled";
-import React, { useRef } from "react";
+import styles from "./imageFileInput.module.css";
+import React, { useRef, useState } from "react";
 
 const ImageFileInput = ({ imageUploader, name, onFileChange }) => {
+  const [loading, setLoading] = useState(false);
+
   const inputRef = useRef();
   const onButtonClick = (event) => {
     event.preventDefault();
@@ -9,7 +13,9 @@ const ImageFileInput = ({ imageUploader, name, onFileChange }) => {
   };
 
   const onChange = async (event) => {
+    setLoading(true);
     const uploaded = await imageUploader.upload(event.target.files[0]);
+    setLoading(false);
     onFileChange({
       name: uploaded.original_filename,
       url: uploaded.url,
@@ -24,7 +30,15 @@ const ImageFileInput = ({ imageUploader, name, onFileChange }) => {
         name="file"
         onChange={onChange}
       />
-      <Button onClick={onButtonClick}>{name || "No File"}</Button>
+      {!loading && (
+        <button
+          className={`${styles.button} ${name ? styles.pink : styles.grey}`}
+          onClick={onButtonClick}
+        >
+          {name || "No File"}
+        </button>
+      )}
+      {loading && <Loading></Loading>}
     </Container>
   );
 };
@@ -33,23 +47,31 @@ const Container = styled.div`
   width: 100%;
   height: 100%;
   display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Input = styled.input`
   display: none;
 `;
 
-const Button = styled.button`
-  width: 100%;
-  backgroud-color: black;
-  font-weight: bold;
-  border: 0;
-  outline: 0;
-  font-size: 0.8rem;
+const spin = keyframes`
+0% {
+  transform: rotate(0deg);
+}
 
-  &:hover {
-    opacity: 0.8;
-  }
+100% {
+  transform: rotate(360deg);
+}
+`;
+
+const Loading = styled.div`
+  width: 1.7rem;
+  height: 1.7rem;
+  border-radius: 50%;
+  border: 3px solid #f7f8f8;
+  border-top: 3px solid #fbc7d4;
+  animation: ${spin} 2s linear infinite;
 `;
 
 export default ImageFileInput;
